a677a061cbfac399c07629712bc086833645640c
operhero <648795307@qq.com>
2019-04-16 15:51:00 +0800

    first commit

---
+++ src/main/java/com/operhero/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.operhero.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/hello";
+    }
+}
